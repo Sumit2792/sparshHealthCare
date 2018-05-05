ce4be465150c34fc954a7dea33889643234cd63f
Sumit Srivastava <srivastavasumit92@gmail.com>
2018-05-05 23:12:17 +0530

    First commit

---
+++ src/main/java/com/SHC/SparshHealthCareApplication.java
@@ -0,0 +1,12 @@
+package com.SHC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparshHealthCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparshHealthCareApplication.class, args);
+	}
+}
